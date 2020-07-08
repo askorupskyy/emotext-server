4861c0900bc9730824b94d1e73f625fa9fa90af0
rcbxd <rbcxdd@gmail.com>
2020-07-07 19:54:26 -0500

    db migrations

--- server/util/db.js
+++ server/util/db.js
@@ -1,6 +1,6 @@
-const { DB_USER, DB_PASSWORD } = require('../cfg');
+const { DB_HOST, DB_PORT, DB_SSL, DB_USER, DB_PASSWORD } = require('../cfg');
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('postgres',
+const sequelize = new Sequelize('messagingapp',
     DB_USER || 'postgres',
     DB_PASSWORD || '',
     {
@@ -8,7 +8,7 @@ const sequelize = new Sequelize('postgres',
         port: DB_PORT || 5432,
         dialect: 'postgres',
         dialectOptions: {
-            ssl: process.env.DB_SSL == "true"
+            ssl: DB_SSL == "true"
         }
     });
 
