df420441df7c7dd48cd256f8ecb74a3ff1c74e86
rcbxd <rbcxdd@gmail.com>
2020-07-30 17:00:18 -0500

    migrations on startup

--- util/migrate.js
+++ util/migrate.js
@@ -1,11 +1,14 @@
 const db = require('./db');
 
 const Chat = require('../models/Chat');
+const Contact = require('../models/Contact');
 const EmailVerificationCode = require('../models/EmailVerificationCode');
-const Friendship = require('../models/Friendship');
+const FriendRequest = require('../models/FriendRequest');
+const GroupChat = require('../models/GroupChat');
 const Message = require('../models/Message');
 const PasswordResetCode = require('../models/PasswordResetCode');
 const User = require('../models/User');
+const UserRestrictions = require('../models/UserRestrictions');
 const UserSession = require('../models/UserSession');
 
 db.sync();
