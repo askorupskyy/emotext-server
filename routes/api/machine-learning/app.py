090fda6700459a6c53f23be49a67311d15845f2f
rcbxd <rbcxdd@gmail.com>
2020-11-24 16:25:24 -0600

    add predict api

--- routes/api/machine-learning/app.py
+++ routes/api/machine-learning/app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, request
+from flask.json import jsonify
+from models.model_predict import Model
+
+import os
+
+MESSAGES_MODEL = Model(os.path.abspath(
+    "routes/api/machine-learning/models/emotext.tf"))
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    prediction = MESSAGES_MODEL.predict(request.get_json()['texts'])
+    return jsonify({"success": True, "message": ("negative" if prediction > 0.5 else "positive")})
 
 
 if __name__ == '__main__':
