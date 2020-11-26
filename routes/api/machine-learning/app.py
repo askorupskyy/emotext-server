4dabd519c85944034f2200e087c82ffef92744c2
devr0306 <devr0306@gmail.com>
2020-11-25 18:38:50 -0600

    Added the ability to send in a string and predict sentiment

--- routes/api/machine-learning/app.py
+++ routes/api/machine-learning/app.py
@@ -16,9 +16,21 @@ def hello_world():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    prediction = MESSAGES_MODEL.predict(request.get_json()['texts'])
+    
+    texts = request.get_json()
+
+    textStrings = texts['texts']
+    textsRay = ("" + textStrings).split("||")
+    prediction = MESSAGES_MODEL.predict(textsRay)
     return jsonify({"success": True, "message": ("negative" if prediction > 0.5 else "positive")})
 
 
+@app.route('/predict_list', methods=['POST'])
+def predict():
+    
+    prediction = MESSAGES_MODEL.predict(request.get_json()['texts'])
+    return jsonify({"success": True, "message": ("negative" if prediction > 0.5 else "positive")})
+
 if __name__ == '__main__':
+    app.debug = True
     app.run(host="localhost", port=8000, debug=True)
